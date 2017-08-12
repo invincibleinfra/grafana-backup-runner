87696446eb42de73b30520c0dc07c5c2441b99cb
Sam Manzer <github@invincibleinfra.com>
2017-08-12 18:06:04 -0500

    skeleton with flags, and also change builder image

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
 REPO_URL = github.com/invincibleinfra/grafana-backup-runner
 
 # Build using golang docker container for reproducibility
-GOLANG_IMAGE=golang:1.8.3-jessie
+# this image also includes things we need like glide
+GOLANG_IMAGE=amaysim/golang:1.8.3
 CONTAINER_GOPATH=/go
 CONTAINER_SOURCE_DIR=$(CONTAINER_GOPATH)/src/$(REPO_URL)
 
 build:
 	docker run \
+		-e "CGO_ENABLED=0" \
 		-v $(CURDIR):$(CONTAINER_SOURCE_DIR) \
 		-w $(CONTAINER_SOURCE_DIR) \
 		--rm $(GOLANG_IMAGE) \
-		go build -o grafana-backup-runner
+		/bin/bash -c "glide install && go build -o grafana-backup-runner"
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import "flag"
+import "gopkg.in/asaskevich/govalidator.v6"
+import "log"
+import "os"
+import "strings"
 
 func main() {
-	fmt.Println("vim-go")
+	grafanaURLRaw := flag.String("grafanaURL", "", "The URL of the grafana server to be backed up")
+	s3BucketURLRaw := flag.String("s3BucketURL", "", "The URL of the S3 bucket where the backup should be stored")
+	flag.Parse()
+
+	if *grafanaURLRaw == "" || *s3BucketURLRaw == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if !govalidator.IsURL(*grafanaURLRaw) {
+		log.Fatalf("Invalid grafanaURL: %v", *grafanaURLRaw)
+	}
+
+	// validator cannot handle 's3://' type URLs
+	tmpS3BucketURL := strings.Replace(*s3BucketURLRaw, "s3:", "http:", 1)
+	if !govalidator.IsURL(tmpS3BucketURL) {
+		log.Fatalf("Invalid s3BucketURL: %v", *s3BucketURLRaw)
+	}
 }
